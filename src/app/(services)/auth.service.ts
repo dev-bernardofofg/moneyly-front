fa542be922610259307024d5fcd5906075590fdc
dev-bernardofofg <dev.bernardofofg@gmail.com>
2025-06-26 06:27:59 -0300

    refactor: consolidate authentication requests into auth.service.ts
    - Removed auth.http.ts file and migrated SignInRequest and SignUpRequest functions to auth.service.ts.
    - Updated import paths for consistency and clarity.
    - This change simplifies the authentication logic by centralizing it within the auth service, improving maintainability.

--- src/app/(services)/auth.service.ts
+++ src/app/(services)/auth.service.ts
@@ -1,5 +1,7 @@
-import { AuthResponse, SignInParams, SignUpParams } from "../(types)/auth";
-import api from "../(utils)/axios";
+import { AuthResponse, SignInParams, SignUpParams } from "@/app/(types)/auth";
+import api from "@/app/(utils)/axios";
+import { useMutation, UseMutationOptions } from "@tanstack/react-query";
+import { AxiosError } from "axios";
 
 export const authService = {
   signIn: async (params: SignInParams): Promise<AuthResponse> => {
@@ -13,4 +15,32 @@ export const authService = {
 
     return response.data;
   },
+};
+
+export const SignInRequest = (
+  options?: UseMutationOptions<AuthResponse, AxiosError, SignInParams>
+) => {
+  return useMutation<AuthResponse, AxiosError, SignInParams>({
+    mutationFn: authService.signIn,
+    onSuccess: (data, variables, context) => {
+      options?.onSuccess?.(data, variables, context);
+    },
+    onError: (data, variables, context) => {
+      options?.onError?.(data, variables, context);
+    },
+  });
+};
+
+export const SignUpRequest = (
+  options?: UseMutationOptions<AuthResponse, AxiosError, SignUpParams>
+) => {
+  return useMutation<AuthResponse, AxiosError, SignUpParams>({
+    mutationFn: authService.signUp,
+    onSuccess: (data, variables, context) => {
+      options?.onSuccess?.(data, variables, context);
+    },
+    onError: (data, variables, context) => {
+      options?.onError?.(data, variables, context);
+    },
+  });
 };
