54a9579b4b5037338802cf2a899c00d7a04a6d5e
dev-bernardofofg <dev.bernardofofg@gmail.com>
2026-05-22 19:50:43 -0300

    feat(section): add className and classNameHeader props

--- src/app/(components)/(bases)/(cards)/section.tsx
+++ src/app/(components)/(bases)/(cards)/section.tsx
@@ -1,34 +1,33 @@
-"use client"
+'use client';
 
-import { StaggeredFade } from '@/app/(components)/(motions)/staggered-fade'
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
-import { LucideIcon } from 'lucide-react'
+import { StaggeredFade } from '@/app/(components)/(motions)/staggered-fade';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { cn } from '@/lib/utils';
+import { LucideIcon } from 'lucide-react';
 
 interface SectionProps {
-  Icon: LucideIcon
-  title: string
-  children: React.ReactNode
+  Icon: LucideIcon;
+  title: string;
+  children: React.ReactNode;
+  className?: string;
+  classNameHeader?: string;
 }
 
-export const Section = ({
-  Icon,
-  title,
-  children
-}: SectionProps) => {
+export const Section = ({ Icon, title, children, className, classNameHeader }: SectionProps) => {
   return (
-    <StaggeredFade className='bg-slate-100 dark:bg-slate-800 rounded-lg dark:border-slate-700 border border-slate-200'>
-      <Card className='w-full border-none'>
-        <CardHeader className='border-b border-slate-200 dark:border-slate-700'>
-          <CardTitle className='flex items-center gap-2'>
-            <Icon className='size-5 text-primary' />
-            <span className='text-lg font-bold'>{title}</span>
+    <StaggeredFade className="bg-slate-100 dark:bg-slate-800 rounded-lg dark:border-slate-700 border border-slate-200">
+      <Card className="w-full border-none">
+        <CardHeader
+          className={cn('border-b border-slate-200 dark:border-slate-700', classNameHeader)}
+        >
+          <CardTitle className="flex items-center gap-2">
+            <Icon className="size-5 text-primary" />
+            <span className="text-lg font-bold">{title}</span>
           </CardTitle>
         </CardHeader>
 
-        <CardContent className="pt-4">
-          {children}
-        </CardContent>
+        <CardContent className={cn('pt-4', className)}>{children}</CardContent>
       </Card>
     </StaggeredFade>
-  )
-} 
+  );
+};
